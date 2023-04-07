86b51dd0349c68c5bd45e27ce71f7fab5e0ccc92
Hyboy95 <temilator@gmail.com>
2023-04-07 13:39:02 +0700

    .\BT_thiết_kế_và_triển_khai_lớp_triagle\

--- BT_thiết_kế_và_triển_khai_lớp_triagle/main.ts
+++ BT_thiết_kế_và_triển_khai_lớp_triagle/main.ts
@@ -1,6 +1,11 @@
 import {Triangle} from "./Triangle";
+import {Shape} from "./Shape";
 
+let shape: Shape = new Shape('hinh_1','blue');
 let triangle: Triangle = new Triangle('tam giac 1', 'red', 3,3,3);
+console.log(shape);
+console.log(shape.getName());
+console.log(shape.getColor());
 console.log(triangle);
 console.log(triangle.getArea());
 console.log(triangle.getPerimeter());
